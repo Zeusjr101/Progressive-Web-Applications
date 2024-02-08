14cf66c9158ac907cbccea42299463e6ab441c84
JesusJr <Jesusujr101@gmail.com>
2024-02-07 19:38:06 -0500

    made changes in database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,8 @@ const initdb = async () =>
     },
   });
     // TODO: Add logic to a method that accepts some content and adds it to the database
-    export const putDb = async (content) => console.error("putDb not implemented");
-    {
+    export const putDb = async (content) =>{ console.error("putDb not implemented");
+    
       const dataDb = await openDB("jate", 1);
       const tx = dataDb.transaction("jate", "readwrite");
       const store = tx.objectStore("jate");
@@ -23,8 +23,8 @@ const initdb = async () =>
     };
   
     // TODO: Add logic for a method that gets all the content from the database
-    export const getDb = async () => console.error("getDb not implemented");
-    {
+    export const getDb = async () => {console.error("getDb not implemented");
+    
       const dataDb = await openDB("jate", "1");
       const tx = dataDb.transaction("jate", "readonly");
       const store = tx.objectStore("jate");
